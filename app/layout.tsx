ae15319606783c06549c8fb000eebb00044844a2
rescueslovakia-ctrl <rescueslovakia@gmail.com>
2026-08-22 22:42:45 +0200

    Add application layout

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"DuoAkva Diary",description:"Inteligentný akvaristický denník"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="sk"><body>{children}</body></html>}
